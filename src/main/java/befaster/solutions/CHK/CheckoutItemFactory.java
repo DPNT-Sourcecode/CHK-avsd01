63682a16811cc54d71bffca168d126e058c04331
sbx_user1051 <sbx_user1051@169.254.248.37>
2019-06-13 14:44:41 +0000

    Thu Jun 13 14:44:41 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutItemFactory.java
+++ src/main/java/befaster/solutions/CHK/CheckoutItemFactory.java
@@ -28,8 +28,7 @@ public class CheckoutItemFactory {
     }
     
     /**
-     * Our price table and offers: 
-    +------+-------+------------------------+
+   +------+-------+------------------------+
     | Item | Price | Special offers         |
     +------+-------+------------------------+
     | A    | 50    | 3A for 130, 5A for 200 |
@@ -38,6 +37,26 @@ public class CheckoutItemFactory {
     | D    | 15    |                        |
     | E    | 40    | 2E get one B free      |
     | F    | 10    | 2F get one F free      |
+    | G    | 20    |                        |
+    | H    | 10    | 5H for 45, 10H for 80  |
+    | I    | 35    |                        |
+    | J    | 60    |                        |
+    | K    | 80    | 2K for 150             |
+    | L    | 90    |                        |
+    | M    | 15    |                        |
+    | N    | 40    | 3N get one M free      |
+    | O    | 10    |                        |
+    | P    | 50    | 5P for 200             |
+    | Q    | 30    | 3Q for 80              |
+    | R    | 50    | 3R get one Q free      |
+    | S    | 30    |                        |
+    | T    | 20    |                        |
+    | U    | 40    | 3U get one U free      |
+    | V    | 50    | 2V for 90, 3V for 130  |
+    | W    | 20    |                        |
+    | X    | 90    |                        |
+    | Y    | 10    |                        |
+    | Z    | 50    |                        |
     +------+-------+------------------------+
      * @param itemSKU
      * @return 
@@ -63,6 +82,13 @@ public class CheckoutItemFactory {
                         new OtherItemDiscountPack("B", 1)));
             case "F":
                 return new CheckoutItem(itemSKU, 1, 10, new PriceDiscountOffer(3, 20));
+            case "G":
+                return new CheckoutItem(itemSKU, 1, 20);
+            case "H":
+                return new CheckoutItem(itemSKU, 1, 10, new PriceDiscountOffer(5, 45),
+                        new PriceDiscountOffer(10, 80));
+            case "I":
+                return new CheckoutItem(itemSKU, 1, 35);
         }
         
         throw new IllegalArgumentException("Unexpected SKU");
@@ -82,4 +108,5 @@ public class CheckoutItemFactory {
         return new CheckoutCart(currentItemState.values());
     }
 }
+
 
