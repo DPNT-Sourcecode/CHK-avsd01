0bcc86bf2ae8a27489c8cf6aea3caa41a966d713
sbx_user1051 <sbx_user1051@169.254.248.37>
2019-06-13 11:44:15 +0000

    Thu Jun 13 11:44:15 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/CheckoutItem.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package befaster.solutions.CHK;
+
+/**
+ *
+ * @author robert.damian
+ */
+public class CheckoutItem {
+    private final int quantity;
+    private final double price;
+    
+    public CheckoutItem(int quantity, double price) {
+        this.quantity = quantity;
+        this.price = price;
+    }
+    
+    public double getTotal() {
+        return price * quantity;
+    }
+}
