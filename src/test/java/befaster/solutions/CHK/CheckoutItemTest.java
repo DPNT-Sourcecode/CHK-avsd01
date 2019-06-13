b716e7480ab8c711b578b683a74386e633dc0b72
sbx_user1051 <sbx_user1051@169.254.248.37>
2019-06-13 12:08:15 +0000

    Thu Jun 13 12:08:15 UTC 2019

---
+++ src/test/java/befaster/solutions/CHK/CheckoutItemTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package befaster.solutions.CHK;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author robert.damian
+ */
+public class CheckoutItemTest {
+    @Test
+    public void testNormalItem() {
+        CheckoutItem item = new CheckoutItem(5, 40);
+        
+        Assert.assertEquals(200, item.getTotal());
+    }
+    
+    @Test
+    public void testWithOffer() {
+        CheckoutItem item = new CheckoutItem(5, 40, new SpecialOffer(2, 70));
+        
+        Assert.assertEquals(180, item.getTotal());
+    }
+}
